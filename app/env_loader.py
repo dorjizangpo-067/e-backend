670a63f8904a69038ac1ccfde6ea1ac3afbefa93
Dorji Zangpo <dorjizangpo067@gmail.com>
2026-03-02 19:14:41 +0600

    UPDATE: str from secrecstr for secret_key for not showing secrete_key in logs

--- app/env_loader.py
+++ app/env_loader.py
@@ -1,9 +1,10 @@
+from pydantic import SecretStr
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
     postgresql_url: str
-    secret_key: str
+    secret_key: SecretStr
     algorithm: str
     access_token_expire_minutes: int
     admin_email: str
